3da4863feb77b25c872193aadfb5925f41a95b33
Bala Krishna <balakrishna@docsapp.in>
2019-08-21 15:37:21 +0530

    Part 2: Hello World

---
+++ hello/hello.go
@@ -0,0 +1,10 @@
+package main // Used to maintain compartmentalisation and reusability
+
+import "fmt"
+
+func main() { // The main function should always reside in the main package
+	fmt.Print("Yes Mister!")
+	fmt.Println("Hello World! Now without this line, you can't learn a new language, can you?")
+	fmt.Printf("%s", "This is a string")
+
+}
